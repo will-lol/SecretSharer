edc4f9d2a9181611b5a31d24ac55f97337430d3f
William Bradshaw <will.bradshaw50@gmail.com>
2023-04-10 15:40:34 +1000

    lmfao

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import solid from "solid-start/vite";
+import cloudflare from "solid-start-cloudflare-pages";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ adapter: cloudflare({}) })],
 });
