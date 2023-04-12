0e0d2968ae15baf335145f0fd1257f148cf1612e
William Bradshaw <will.bradshaw50@gmail.com>
2023-04-12 15:19:07 +1000

    Cool enookmoji

--- src/routes/api/secrets/[id].ts
+++ src/routes/api/secrets/[id].ts
@@ -17,22 +17,7 @@ const dbReturn = z.object({
 });
 
 const tokenShape = z.object({ current: z.string(), next: z.string() });
-
 let signingKey: ReturnType<typeof tokenShape.parse> | undefined = undefined;
-
-export async function getSigningKeys() {
-  const req = await fetch("https://qstash.upstash.io/v1/keys", {
-    headers: { "Authorization": `Bearer ${process.env.QSTASH_TOKEN}` },
-  })
-  console.log(req);
-  console.log(await req.text());
-  const parse = tokenShape.safeParse(await req.json());
-  if (parse.success) {
-    return parse.data;
-  } else {
-    throw("uhho");
-  }
-}
 
 export async function GET({ params }: APIEvent) {
   const parameter = params.id;
@@ -54,6 +39,40 @@ export async function GET({ params }: APIEvent) {
   return new Response("ID not found", { status: 404 });
 }
 
+postFake(
+  { id: "26b51998-ddcf-4720-b10a-6ad4ee30b123" },
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIiLCJib2R5IjoiNDdERVFwajhIQlNhLV9USW1XLTVKQ2V1UWVSa201Tk1wSldaRzNoU3VGVT0iLCJleHAiOjE2ODEyNzYzNDgsImlhdCI6MTY4MTI3NjA0OCwiaXNzIjoiVXBzdGFzaCIsImp0aSI6Imp3dF82dnMxYTNxUWhNOHJTTlg0NWNwRVNOZEdBQWtGIiwibmJmIjoxNjgxMjc2MDQ4LCJzdWIiOiJodHRwczovL3NlY3JldC1zaGFyZXIudmVyY2VsLmFwcC9hcGkvc2VjcmV0cy8yNmI1MTk5OC1kZGNmLTQ3MjAtYjEwYS02YWQ0ZWUzMGIxMjMifQ.G3CJjEeTLHVqmm37eB_nmoH7-9XvoG8kUaXh1x3spMs"
+);
+
+async function postFake(params: { [key: string]: string }, signature: string) {
+  {
+    if (signature) {
+      if (signingKey == undefined) {
+        signingKey = tokenShape.parse(
+          await fetch("https://qstash.upstash.io/v1/keys", {
+            headers: { Authorization: `Bearer ${process.env.QSTASH_TOKEN}` },
+          }).then((res) => res.json())
+        );
+      }
+      const secret = new TextEncoder().encode(signingKey!.current);
+      const { payload, protectedHeader } = await jwtVerify(signature, secret);
+      console.log(payload);
+      console.log(protectedHeader);
+    }
+  }
+
+  const parameter = params.id;
+  const conn = connect(config);
+  const resultsDelete = await conn.execute(
+    'DELETE FROM Secrets WHERE SecretID="' + parameter + '"'
+  );
+  if (resultsDelete.rowsAffected == 1) {
+    return new Response("Success", { status: 200 });
+  } else {
+    return new Response("ID not found", { status: 404 });
+  }
+}
+
 export async function POST(event: APIEvent) {
   const params = event.params;
 
@@ -61,15 +80,15 @@ export async function POST(event: APIEvent) {
     const signature = event.request.headers.get("upstash-signature");
     console.log(signature);
     if (signature) {
-      if (signingKey = undefined) {
+      if ((signingKey = undefined)) {
         signingKey = await getSigningKeys();
       }
       console.log(signingKey);
-      const secret = new TextEncoder().encode((signingKey!.current));
+      const secret = new TextEncoder().encode(signingKey!.current);
       console.log(secret);
-      const {payload, protectedHeader} = await jwtVerify(signature, secret);
+      const { payload, protectedHeader } = await jwtVerify(signature, secret);
       console.log(payload);
-      console.log(protectedHeader);  
+      console.log(protectedHeader);
     }
   }
 
