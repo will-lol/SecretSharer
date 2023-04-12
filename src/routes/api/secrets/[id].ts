d6ea5c6e1d80337e25f68d063795150142e3c06e
William Bradshaw <will.bradshaw50@gmail.com>
2023-04-12 13:24:44 +1000

    cool emoji

--- src/routes/api/secrets/[id].ts
+++ src/routes/api/secrets/[id].ts
@@ -2,44 +2,72 @@ import { APIEvent, json } from "solid-start/api";
 import "dotenv/config";
 import { connect } from "@planetscale/database";
 import { z } from "zod";
+import { jwtVerify } from "jose";
 
 export const config = {
   host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  runtime: 'edge'
+  runtime: "edge",
 };
 
 const dbReturn = z.object({
-    SecretID: z.string(),
-    Data: z.string()
-})
+  SecretID: z.string(),
+  Data: z.string(),
+});
+
+const tokenShape = z.object({ current: z.string(), next: z.string() });
+let upstashSigningKey = await getSigningKeys();
+
+async function getSigningKeys() {
+  return tokenShape.parse(
+    await fetch("https://qstash.upstash.io/v1/keys", {
+      headers: { Authorization: `Bearer ${process.env.QSTASH_TOKEN}` },
+    }).then((res) => res.json())
+  );
+}
 
 export async function GET({ params }: APIEvent) {
   const parameter = params.id;
   const conn = connect(config);
-  const results = await conn.execute("SELECT * FROM Secrets WHERE SecretID=\"" + parameter + "\"");
-  const resultsDelete = await conn.execute("DELETE FROM Secrets WHERE SecretID=\"" + parameter + "\"");
+  const results = await conn.execute(
+    'SELECT * FROM Secrets WHERE SecretID="' + parameter + '"'
+  );
+  const resultsDelete = await conn.execute(
+    'DELETE FROM Secrets WHERE SecretID="' + parameter + '"'
+  );
 
   if (results.rows[0] && resultsDelete.rowsAffected == 1) {
     const resultFinal = dbReturn.safeParse(results.rows[0]);
     if (resultFinal.success) {
-        return json(resultFinal.data);
+      return json(resultFinal.data);
     }
   }
 
-  return new Response("ID not found", {status: 404})
+  return new Response("ID not found", { status: 404 });
 }
 
 export async function POST(event: APIEvent) {
   const params = event.params;
-  console.log(event.request.headers.get("upstash-signature"));
+
+  {
+    const signature = event.request.headers.get("upstash-signature");
+    if (signature) {
+      const secret = new TextEncoder().encode(upstashSigningKey.current);
+      const {payload, protectedHeader} = await jwtVerify(signature, secret)
+      console.log(payload);
+      console.log(protectedHeader);  
+    }
+  }
+  
   const parameter = params.id;
   const conn = connect(config);
-  const resultsDelete = await conn.execute("DELETE FROM Secrets WHERE SecretID=\"" + parameter + "\"");
+  const resultsDelete = await conn.execute(
+    'DELETE FROM Secrets WHERE SecretID="' + parameter + '"'
+  );
   if (resultsDelete.rowsAffected == 1) {
-    return new Response("Success", {status: 200});
+    return new Response("Success", { status: 200 });
   } else {
-    return new Response("ID not found", {status: 404});
+    return new Response("ID not found", { status: 404 });
   }
 }
