c0b5b1626ce9c9b2cad23442c18e62e17d0f42c8
William Bradshaw <will.bradshaw50@gmail.com>
2023-04-13 15:25:48 +1000

    rate limit

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,17 +9,26 @@ import { Redis } from "@upstash/redis";
 const rateLimiter = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(1, "5s"),
-  prefix: "@upstash/ratelimit"
-})
+  prefix: "@upstash/ratelimit",
+});
 
 export default createHandler(
   ({ forward }) => {
-    return async event => {
-      console.log(event.clientAddress);
-      const req = event.request;
-      const id = req.headers.get("x-forwarded-for");
-      return forward(event);
-    }
+    return async (event) => {
+      const url = new URL(event.request.url);
+      if (url.pathname.includes("/api/")) {
+        const id = event.clientAddress;
+        const rateLimitResult = await rateLimiter.limit(id);
+
+        if (!rateLimitResult.success) {
+          return new Response("Couldn't process request", { status: 429 });
+        }
+
+        return forward(event);
+      } else {
+        return forward(event);
+      }
+    };
   },
   renderAsync((event) => <StartServer event={event} />)
 );
